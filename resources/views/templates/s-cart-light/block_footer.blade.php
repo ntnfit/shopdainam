426de6f5a4fe335ac340fe6fca1649570107cd10
Thanh Nguyen Nguyen <ntnfit98@gmail.com>
2022-06-22 00:03:09 +0700

    update google analyisc

--- resources/views/templates/s-cart-light/block_footer.blade.php
+++ resources/views/templates/s-cart-light/block_footer.blade.php
@@ -92,13 +92,4 @@
 
   gtag('config', 'G-HLYKC6LD57');
 </script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PPBRWTE4LH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PPBRWTE4LH');
-</script>
 </footer>
